c4950d109db3a88c272b99faab2751b36a905e57
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2020-05-12 22:37:37 +0530

    single page application part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,8 +7,9 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'contactus', component: ContactComponent },
-    { path: 'menu', component: MenuComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
-];
+         { path: "home", component: HomeComponent },
+         { path: "contactus", component: ContactComponent },
+         { path: "dishdetail/:id", component: DishdetailComponent },
+         { path: "menu", component: MenuComponent },
+         { path: "", redirectTo: "/home", pathMatch: "full" },
+       ];
